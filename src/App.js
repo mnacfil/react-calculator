36c2fa9a584af27b5e8b370a852660eef6d360dc
Melvin Nacfil <you@example.com>
2023-03-07 10:49:14 +0800

    Finish functionality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import { useReducer } from "react";
 import DigitButton from "./components/DigitButton";
 import OperationButton from './components/OperationButton';
 import { reducer } from "./reducer";
-import {CLEAR, DELETE_DIGIT} from './action'
+import {CLEAR, DELETE_DIGIT, CALCULATE} from './action'
+
+// if 12.0000 format it to just 12
+const NUMBER_FORMATTER = new Intl.NumberFormat('es-US', {
+  maximumFractionDigits: 0
+})
+
+const formatOperand = (operand) => {
+  console.log(operand);
+  if(!operand) return;
+  if(!operand.toString().includes('.')) {
+    return NUMBER_FORMATTER.format(operand);
+  }
+  const [integer, decimal] = operand.toString().split('.');
+  return `${NUMBER_FORMATTER.format(integer)}.${decimal}`;
+}
+
 
 function App() {
   const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {});
+
   return (
     <div className="calculator">
       <div className="output">
-        <div className="previous-operand">{previousOperand} {operation}</div>
-        <div className="current-operand">{currentOperand}</div>
+        <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
       <button className="span-two" onClick={() => dispatch({ type: CLEAR})}>AC</button>
       <button onClick={() => dispatch({ type: DELETE_DIGIT})}>DEL</button>
@@ -30,7 +47,7 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch}/>
       <DigitButton digit='.' dispatch={dispatch}/>
       <DigitButton digit='0' dispatch={dispatch}/>
-      <button className="span-two">=</button>
+      <button className="span-two" onClick={() => dispatch({ type: CALCULATE})}>=</button>
     </div>
   );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,17 @@
 import { ADD_DIGIT, DELETE_DIGIT, CALCULATE, CLEAR, CHOOSE_OPERATOR} from './action'
 
 export const reducer = (state, { type, payload }) => {
-    const { currentOperand, prevousOperand, operation } = state;
-    console.log(currentOperand);
-    console.log(prevousOperand);
+    const { currentOperand, previousOperand, operation } = state;
     switch(type) {
         case ADD_DIGIT:
-            if(payload.digit === '.' && currentOperand.includes('.')) {
+            if(state.isCalculated) {
+                return {
+                    ...state,
+                    isCalculated: false,
+                    currentOperand: payload.digit
+                }
+            }
+            if(currentOperand && currentOperand.includes('.') && payload.digit === '.') {
                 return state
             }
             if(currentOperand === '0' && payload.digit === '0') {
@@ -19,33 +24,70 @@ export const reducer = (state, { type, payload }) => {
         case CLEAR:
             return {}
         case DELETE_DIGIT:
-            if(currentOperand.length === 1) return {}
+            if(!currentOperand) return state;
+            if(currentOperand.length === 1) {
+                return {
+                    ...state,
+                    currentOperand: null
+                }
+            }
             return {
                 ...state,
                 currentOperand: currentOperand.slice(0, -1)
             }
         case CHOOSE_OPERATOR:
-            if(!currentOperand && !prevousOperand) return state
-            // if(!currentOperand) {
-            //     return {
-            //         ...state,
-            //         operation: payload.operation
-            //     }
-            // }
-            if(!prevousOperand) {
-                console.log("hello");
+            if(!currentOperand && !previousOperand) return state
+            if(!currentOperand) {
+                return {
+                    ...state,
+                    operation: payload.operation
+                }
+            }
+            if(!previousOperand) {
                 return {
                     ...state,
                     operation: payload.operation,
-                    prevousOperand: currentOperand,
+                    previousOperand: currentOperand,
                     currentOperand: null
                 }
             }
-            // return {
-            //     ...state,
-            //     operation: payload.operation,
-            //     prevousOperand: currentOperand,
-            //     currentOperand: null
-            // }
+            if(previousOperand && currentOperand === '.') return state
+            return {
+                ...state,
+                operation: payload.operation,
+                previousOperand: calculate(state),
+                currentOperand: null
+            }
+        case CALCULATE:
+            if(previousOperand && currentOperand === '.') return state
+            return {
+                ...state,
+                operation: null,
+                previousOperand: null,
+                currentOperand: calculate(state),
+                isCalculated: true
+            }
     }
+}
+
+function calculate ({ previousOperand, currentOperand, operation}) {
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if(isNaN(prev) || isNaN(current)) return ''
+    let result;
+    switch(operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '÷':
+            result = prev / current;
+            break;
+    }
+    return result;
 }
